468eceac24c447703febbb8491d8f834f4d5a966
maiabennett <123126475+maiabennett@users.noreply.github.com>
2023-07-11 13:52:59 -0500

    Add files via upload

---
+++ BugSeq-er/normalized_srs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module load qiime2/2021.4
+
+qiime srs SRS   \
+    --i-table artifacts/table.qza   \
+    --p-c-min 3800   \
+    --o-normalized-table artifacts/norm-table.qza   \
+    --verbose
+    
+qiime feature-table summarize \
+    --i-table artifacts/norm-table.qza \
+    --o-visualization artifacts/norm-table.qzv
